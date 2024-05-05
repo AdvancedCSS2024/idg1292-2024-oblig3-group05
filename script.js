a7f1fdfb7633549eb1491ce73d24ff3d0db1d78d
miasannstud <miasann@stud.ntnu.com>
2024-05-05 14:13:04 +0200

    YESSSS FIKK TIL JS

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
-const options = { 
-    root: document.querySelector(".section--1"),
-	rootMargin: "-20px",
-	threshold: [1]
-};
+// DOMContentLoaded for å passe på at dom loader før js.
+document.addEventListener('DOMContentLoaded', function() {
+	
+    const sections = document.querySelectorAll("section")
+	const faders = document.querySelectorAll(".fade-in");
 
-const observer = new IntersectionObserver(cb, options); 
+	const options = {
+		root: null,
+		threshold: 0.25,
+		rootMargin: "10px"
+	};
+
+    const observer = new IntersectionObserver(function
+	(entries, observer) {
+        entries.forEach(entry => {
+
+			if(entry.isIntersecting){
+				console.log(entry.target);
+				entry.target.classList.add("inverse");
+			}else{
+				entry.target.classList.remove("inverse");
+			}
+        });
+    }, options);
+
+	sections.forEach(section => {
+		observer.observe(section);
+	})
+
+	const appearOptions = {
+		threshold: 1,
+		rootMargin: "0px"
+	  };
+	  
+	  const appearOnScroll = new IntersectionObserver(function(
+		entries,
+		appearOnScroll
+	  ) {
+		entries.forEach(entry => {
+		  if (!entry.isIntersecting) {
+			return;
+		  } else {
+			entry.target.classList.add("appear");
+			appearOnScroll.unobserve(entry.target);
+		  }
+		});
+	  },
+	  appearOptions);
+	  
+	  faders.forEach(fader => {
+		appearOnScroll.observe(fader);
+	  });
+});
