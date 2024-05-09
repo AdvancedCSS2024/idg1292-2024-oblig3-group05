e6cd12cce104dd06d4f2763395fc4809e6fbf584
miasannstud <miasann@stud.ntnu.com>
2024-05-09 11:03:28 +0200

    script comt and missing svg

--- script.js
+++ script.js
@@ -34,9 +34,20 @@ document.addEventListener('DOMContentLoaded', function() {'use strict';
         observer.observe(section);
     });
 
+// -- Call to action --
+    // This section of the script contains all the JS pertaining the call to action button.
+    // This removes the trash in the ocean and beach, and changes the color theme of the website.
+
+    // Declares a variable called "cleanupButton" which selects the button
+    // element from the HTML document by its class name
     const cleanupButton = document.getElementById('cleanocean');
 
+    // Adds an event listener to the button which will trigger a function
+    // when the button is clicked
     cleanupButton.addEventListener('click', function() {
+
+        // Selects all elements from the HTML document that we want the
+        // script to interact with and stores them in variables
         const backGrounds = document.querySelector('.section--1');
         const oceanNasty = document.querySelector('.beach-wave--nasty');
         const oceanClean = document.querySelector('.beach-wave');
@@ -47,9 +58,14 @@ document.addEventListener('DOMContentLoaded', function() {'use strict';
         const coralLeftClean = document.querySelector('.coral--left');
         const coralRightClean = document.querySelector('.coral--right');
         const waterGradient = document.querySelector('.underwater-gradient');
+
+        // Selects all HTML elements with the class "garbage" and stores them in an array
         const cleanTheOceanElArr = document.querySelectorAll('.garbage');
+
         const fishGoAway = document.querySelector('.fish--dead');
         
+        // Adds an additional class to all the elements selected previously
+        // which has the property of display: none
         backGrounds.classList.add("niceweather");
         oceanNasty.classList.add("nastywaves");
         oceanClean.classList.add("cleanwaves");
@@ -61,12 +77,19 @@ document.addEventListener('DOMContentLoaded', function() {'use strict';
         coralLeftClean.classList.remove("nastyCorals");
         coralRightClean.classList.remove("nastyCorals");
         waterGradient.classList.add('unpolluted');
+
+        // For each element in variable, add class "cleanTrash" to class list
+        // which has an animation property, to reflect the items being "cleaned up" from the ocean
         cleanTheOceanElArr.forEach(el => el.classList.add('cleanTrash'));
 
+        // Function to add class "cleanedTrash" to each element in the array "cleanTheOceanElArr"
+        // after a 3 second delay (3000 = ms), in order to give time for the "cleanTrash" animation to run
+        // before the items are removed from the document flow 
         setTimeout(() => {
             cleanTheOceanElArr.forEach(el => el.classList.add('cleanedTrash'));
         }, 3000);
     });
+    // -- Call to action END --
 
 	// intersection obererver settings for appearonscroll
     const appearOptions = {
